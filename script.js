dbec9510500f0022196ad7c9b3e611a0c26b013a
hubkrecz <107886221+hubkrecz@users.noreply.github.com>
2022-08-24 19:27:41 +0200

    adding logic

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const goLeft = document.querySelectorAll('.review-buttons-menu')
+const goLeftPressed = () => {
+    console.log("udało się !!");
+}
+goLeft.addEventListener('click', () => {
+    goLeftPressed();
+})
+
+console.log('dddddduupa');
