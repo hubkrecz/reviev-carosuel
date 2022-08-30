558178855604cbec3a47cb3a4b1c5194891efe29
hubkrecz <107886221+hubkrecz@users.noreply.github.com>
2022-08-30 20:12:46 +0200

    adding swaping

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 
 const goLeft = document.querySelector('.review-buttons-GoLeft');
+const goRight = document.querySelector('.review-buttons-GoRight');
+const surpriceMe = document.querySelector('.review-buttons-SurpriceMe');
+
 const review = document.querySelectorAll('.review');
 
 const arrayOfReview = [...review];
 console.log(arrayOfReview);
 
-const goLeftPressed = () => {
-    console.log("udało się !!");
+const number = () => arrayOfReview.filter((current, index) => index > 0);
+
+
+(
+    () => [...number()].forEach((current) => current.classList.add('review--hidden'))
+)();
+
+const reviewSwaper = (index) => {
+    arrayOfReview.forEach((current) => current.classList.add('review--hidden'))
+
+    arrayOfReview[index].classList.remove("review--hidden")
 }
 
+// const indexGiverForRevievSwaper(index) => {
 
+// }
+
+const isContain = (array, className) => array.find((current) => current.contain(!className))
+
+console.log(isContain(number, "reviev--hidden"))
+goLeft.addEventListener("click", () => {
+
+    indexGiverForRevievSwaper()
+})
+goRight.addEventListener("click")
+surpriceMe.addEventListener("click")
